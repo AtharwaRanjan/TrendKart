e9d3cb867093390fb652ce227447f55d18aeec7d
Atharwa Ranjan <hendrix10jul@gmail.com>
2026-06-29 15:01:59 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32 " />
+          <p className="text-gray-400 w-full md:w-2/3">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            euismod, nunc ut
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-567-9980</li>
+            <li>contact@trendkart.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2026@ TrendKart.com- All rights reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
